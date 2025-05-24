e19e738f4a8cfb731db7c9555bfd9b5656954b89
R.Keelan <R.Keelan@gmail.com>
2025-05-24 12:25:39 -0400

    More fixes after manual testing

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,27 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  // Set the project root directory. This is where vite will look for package.json, .env files, etc.
-  root: process.cwd(), // process.cwd() is the default, so this is explicit.
+  // Set the root to the src directory so vite serves files from there
+  root: 'src',
   // Define the entry point for the application. Vite will start processing from this HTML file.
   build: {
     rollupOptions: {
-      input: '/src/index.html' // Path to your main HTML file, relative to the project root.
+      input: {
+        main: resolve(fileURLToPath(new URL('.', import.meta.url)), 'src/index.html'),
+        roulette: resolve(fileURLToPath(new URL('.', import.meta.url)), 'src/roulette.html'),
+        bingo: resolve(fileURLToPath(new URL('.', import.meta.url)), 'src/bingo.html'),
+        canada: resolve(fileURLToPath(new URL('.', import.meta.url)), 'src/canada.html'),
+        schedule: resolve(fileURLToPath(new URL('.', import.meta.url)), 'src/schedule.html'),
+        error: resolve(fileURLToPath(new URL('.', import.meta.url)), 'src/error.html')
+      }
     },
     // Specify the output directory for the build. Files will be placed here after `vite build`.
-    outDir: './dist', 
+    outDir: '../dist', 
   },
   // Configure the development server.
   server: {
-    // If your index.html is not at the root, or you want to serve from a specific path:
-    // open: '/src/index.html', // Automatically open this URL in the browser on server start.
     // configure dev server if needed
   },
   // if you had specific webpack aliases, you might want to replicate them here
